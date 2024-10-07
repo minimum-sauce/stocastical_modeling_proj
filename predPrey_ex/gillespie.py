00bac4bd15a06a8ea6c04142ea15427eb9ca109d
Erik Junsved <dev@erix.dev>
2024-10-07 17:47:00 +0200

    finished first two points code

--- predPrey_ex/gillespie.py
+++ predPrey_ex/gillespie.py
@@ -9,43 +9,44 @@
 import numpy as np
 import random
 
+
 def SSA(prop, stoch, X0, tspan, coeff):
     # prop  - propensities
     # stoch - stiochiometry vector
     # X0    - Initial state vector
     tvec = np.zeros(1)
     tvec[0] = tspan[0]
-    Xarr = np.zeros([1,len(X0)])
-    Xarr[0,:] = X0
+    Xarr = np.zeros([1, len(X0)])
+    Xarr[0, :] = X0
     t = tvec[0]
     X = X0
     sMat = stoch()
-    while t<tspan[1]:
-        r1, r2 = np.random.uniform(0,1, size=2)  # Find two random numbers on uniform distr.
-        re = prop(X,coeff)
+    while t < tspan[1]:
+        r1, r2 = np.random.uniform(0, 1, size=2)  # Find two random numbers on uniform distr.
+        re = prop(X, coeff)
         cre = np.cumsum(re)
         a0 = cre[-1]
         if a0 < 1e-12:
             break
 
-        tau = np.random.exponential(scale=1/a0) # Random number exponential distribution
-        cre=cre/a0
-        r=0
-        while cre[r]<r2:
-            r+=1
-        
-        t+= tau
+        tau = np.random.exponential(scale=1 / a0)  # Random number exponential distribution
+        cre = cre / a0
+        r = 0
+        while cre[r] < r2:
+            r += 1
+
+        t += tau
         # if new time is larger than final time, skip last calculation
         if t > tspan[1]:
             break
-        
-        tvec = np.append(tvec,t)
-        X = X + sMat[r,:]
-        Xarr = np.vstack([Xarr,X])
 
-     # If iterations stopped before final time, add final time and no change
+        tvec = np.append(tvec, t)
+        X = X + sMat[r, :]
+        Xarr = np.vstack([Xarr, X])
+
+    # If iterations stopped before final time, add final time and no change
     if tvec[-1] < tspan[1]:
-        tvec=np.append(tvec,tspan[1])
-        Xarr=np.vstack([Xarr,X])          
-            
+        tvec = np.append(tvec, tspan[1])
+        Xarr = np.vstack([Xarr, X])
+
     return tvec, Xarr
--- src/SIR.py
+++ src/SIR.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python
+
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
-
+from gillespie import SSA
 
 # ----------------------------------- #
 # Deterministic modeling
@@ -11,11 +13,14 @@ S = 0
 I = 1
 R = 2
 
+
 def deltaS(y, beta, population):
-    return -beta * y[I]/population * y[S]
+    return -beta * y[I] / population * y[S]
+
 
 def deltaI(y, gamma, beta, population):
-    return beta * y[I]/population * y[S] - gamma * y[I]
+    return beta * y[I] / population * y[S] - gamma * y[I]
+
 
 def deltaR(y, gamma):
     return gamma * y[I]
@@ -23,17 +28,20 @@ def deltaR(y, gamma):
 
 def ode(t, y, gamma, beta, population):
     return np.array([deltaS(y, beta, population),
-            deltaI(y, gamma, beta, population),
-            deltaR(y, gamma)])
+                     deltaI(y, gamma, beta, population),
+                     deltaR(y, gamma)])
+
 
 beta = 0.3
-gamma = 1/7
-t_span = [0, 120]
+gamma = 1 / 7
+t_span = np.array([0, 120])
 N = 1000
 suseptable = 5
-y0 = [N - suseptable, suseptable, 0]
+y0 = np.array([N - suseptable, suseptable, 0])
 
 sol = solve_ivp(ode, t_span, y0, args=(gamma, beta, N))
+
+plt.figure(1)
 
 plt.plot(sol.t, sol.y[S], label="S(t)")
 plt.plot(sol.t, sol.y[I], label="I(t)")
@@ -42,8 +50,44 @@ plt.plot(sol.t, sol.y[R], label="R(t)")
 plt.xlabel("t")
 plt.xlabel("y")
 plt.legend()
-plt.show()
+
 
 # ----------------------------------- #
 # Stochastical modeling
 # ----------------------------------- #
+
+
+def stoch():
+    m = np.array([[-1, 1, 0],
+                  [0, -1, 1]])
+    return m
+
+
+def propensities(X, coeff):
+    beta = coeff[0]
+    gamma = coeff[1]
+    N = coeff[2]
+
+    w = np.array([
+        beta * X[S] * X[I] / N,
+        gamma * X[I]
+    ])
+    return w
+
+
+X0 = (y0[0], y0[1], y0[2])
+
+coeff = (beta, gamma, N)
+
+t, X = SSA(propensities, stoch, X0, t_span, coeff)
+
+plt.figure(2)
+
+plt.plot(t, X[:, S], label="S(t)")
+plt.plot(t, X[:, I], label="I(t)")
+plt.plot(t, X[:, R], label="R(t)")
+
+plt.xlabel("t")
+plt.xlabel("y")
+plt.legend()
+plt.show()
