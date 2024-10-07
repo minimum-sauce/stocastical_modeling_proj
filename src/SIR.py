671798dd64a4b7aa7fc84345fc21f87f79857d98
Felix Ljungkvist <felixljungkvist@hotmail.com>
2024-10-07 18:36:17 +0200

    working deterministic model form SIR

---
+++ src/SIR.py
@@ -0,0 +1,49 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
+
+# ----------------------------------- #
+# Deterministic modeling
+# ----------------------------------- #
+
+S = 0
+I = 1
+R = 2
+
+def deltaS(y, beta, population):
+    return -beta * y[I]/population * y[S]
+
+def deltaI(y, gamma, beta, population):
+    return beta * y[I]/population * y[S] - gamma * y[I]
+
+def deltaR(y, gamma):
+    return gamma * y[I]
+
+
+def ode(t, y, gamma, beta, population):
+    return np.array([deltaS(y, beta, population),
+            deltaI(y, gamma, beta, population),
+            deltaR(y, gamma)])
+
+beta = 0.3
+gamma = 1/7
+t_span = [0, 120]
+N = 1000
+suseptable = 5
+y0 = [N - suseptable, suseptable, 0]
+
+sol = solve_ivp(ode, t_span, y0, args=(gamma, beta, N))
+
+plt.plot(sol.t, sol.y[S], label="S(t)")
+plt.plot(sol.t, sol.y[I], label="I(t)")
+plt.plot(sol.t, sol.y[R], label="R(t)")
+
+plt.xlabel("t")
+plt.xlabel("y")
+plt.legend()
+plt.show()
+
+# ----------------------------------- #
+# Stochastical modeling
+# ----------------------------------- #
